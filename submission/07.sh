a9fe6d188dedba935ac3bd82315c0b2a54167ab8
DataNerd <lucyk7770@gmail.com>
2025-04-23 14:25:06 +0300

    Update 07.sh

--- submission/07.sh
+++ submission/07.sh
@@ -1,1 +1,3 @@
 # what is the coinbase tx in this block 243,834
+bitcoin-cli -signet getblockhash 243834
+bitcoin-cli -signet getblock 000000a35c00ed62db2379e18a9aa97482cf2a87911d6434eacd810d570c71d3 2
