c74b48500b6f0e91cf51d7cbec927904be8e5d93
DataNerd <lucyk7770@gmail.com>
2025-04-23 13:30:45 +0300

    Update 02.sh

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,3 @@
 # How many new outputs were created by block 243,825?
+bitcoin-cli -signet getblockhash 243825
+bitcoin-cli -signet getblock 0000000e120ff9801366e9c75f1fbe6b72cc6a577bf16a402fd1521c3653f24d 2
