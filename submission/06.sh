7b6c1a645beef3defd5995307c6ba1ab807f4f20
DataNerd <lucyk7770@gmail.com>
2025-04-23 14:21:23 +0300

    Update 06.sh

--- submission/06.sh
+++ submission/06.sh
@@ -1,1 +1,4 @@
 # Only one tx in block 243,821 signals opt-in RBF. What is its txid?
+bitcoin-cli -signet getblockhash 243821
+bitcoin-cli -signet getblock 000000a62319302b80fd00c2abeacdff700ad90b0a86b2b1249a46107162f9a6 2
+
